34c5db5a51f44b82cd1eb396e227b1070acc5c61
Harshthakur21 <78103675+Harshthakur21@users.noreply.github.com>
2024-04-28 01:00:15 +0530

    dcdc

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
   return (
     <div className="App">
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">IVMS</a>
+        <a className="navbar-brand" href="#"></a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
